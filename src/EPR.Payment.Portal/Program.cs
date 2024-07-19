0c84e3501af3254621366c4fd728f799702a09ec
Manish Agarwal <manish.agarwal.external@eviden.com>
2024-07-19 09:05:11 +0100

    Fixed the breaking change and added wwwroot to gitignore

--- src/EPR.Payment.Portal/Program.cs
+++ src/EPR.Payment.Portal/Program.cs
@@ -14,7 +14,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/PaymentError/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=PaymentError}/{action=Index}/{id?}");
 
 app.Run();
