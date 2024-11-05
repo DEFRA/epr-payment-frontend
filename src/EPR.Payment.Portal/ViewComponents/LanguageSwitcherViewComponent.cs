1d3886f6e0ac40705ee275b969e810866a34654a
Manish Agarwal <manish.agarwal.external@eviden.com>
2024-11-05 16:54:50 +0000

    feature/task/AD463678 Welsh and English Toggle

---
+++ src/EPR.Payment.Portal/ViewComponents/LanguageSwitcherViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using EPR.Payment.Portal.ViewModels;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace EPR.Payment.Portal.ViewComponents
+{
+    public class LanguageSwitcherViewComponent : ViewComponent
+    {
+        private readonly IOptions<RequestLocalizationOptions> _localizationOptions;
+
+        public LanguageSwitcherViewComponent(IOptions<RequestLocalizationOptions> localizationOptions)
+        {
+            _localizationOptions = localizationOptions;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cultureFeature = HttpContext.Features.Get<IRequestCultureFeature>();
+            var languageSwitcherModel = new LanguageSwitcherModel
+            {
+                SupportedCultures = _localizationOptions.Value.SupportedCultures!.ToList(),
+                CurrentCulture = cultureFeature!.RequestCulture.Culture,
+                ReturnUrl = $"~{Request.Path}{Request.QueryString}",
+                ShowLanguageSwitcher = true // TODO: Set Feature Flag
+            };
+
+            return View(languageSwitcherModel);
+        }
+    }
+}
