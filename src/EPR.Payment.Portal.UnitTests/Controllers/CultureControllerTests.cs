64e32cdcb3c1a96b4a8ea20aa36dcabaa7e0e353
Manish Agarwal <manish.agarwal.external@eviden.com>
2024-11-06 10:51:48 +0000

    Added unit tests

---
+++ src/EPR.Payment.Portal.UnitTests/Controllers/CultureControllerTests.cs
@@ -0,0 +1,60 @@
+﻿using AutoFixture;
+using EPR.Payment.Portal.Common.Constants;
+using EPR.Payment.Portal.Controllers.Culture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Payment.Portal.UnitTests.Controllers
+{
+    [TestClass]
+    public class CultureControllerTests
+    {
+        private const string ReturnUrl = "returnUrl";
+        private const string CultureEn = "en";
+
+        private Mock<IResponseCookies> _responseCookiesMock = null!;
+        private Mock<ISession> _sessionMock = null!;
+        private Mock<HttpContext> _httpContextMock = null!;
+        private CultureController _systemUnderTest = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _responseCookiesMock = new Mock<IResponseCookies>();
+            _sessionMock = new Mock<ISession>();
+            _httpContextMock = new Mock<HttpContext>();
+
+            // Set up the controller with mock HttpContext
+            _systemUnderTest = new CultureController
+            {
+                ControllerContext = { HttpContext = _httpContextMock.Object }
+            };
+
+            _httpContextMock.Setup(x => x.Session).Returns(_sessionMock.Object);
+        }
+
+        [TestMethod]
+        public void CultureController_UpdateCulture_RedirectsToReturnUrlWithCulture()
+        {
+            // Arrange
+            _httpContextMock
+                .Setup(x => x.Response.Cookies)
+                .Returns(_responseCookiesMock.Object);
+
+            var cultureBytes = Encoding.UTF8.GetBytes(CultureEn);
+
+            // Act
+            var result = _systemUnderTest.UpdateCulture(CultureEn, ReturnUrl);
+
+            // Assert
+            result.Url.Should().Be(ReturnUrl);
+            _sessionMock.Verify(x => x.Set(Language.SessionLanguageKey, cultureBytes), Times.Once);
+        }
+    }
+}
---
+++ src/EPR.Payment.Portal.UnitTests/ViewComponents/LanguageSwitcherViewComponentTests.cs
@@ -0,0 +1,86 @@
+﻿using AutoFixture;
+using EPR.Payment.Portal.Common.Constants;
+using EPR.Payment.Portal.Common.UnitTests.TestHelpers;
+using EPR.Payment.Portal.ViewComponents;
+using EPR.Payment.Portal.ViewModels;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Payment.Portal.UnitTests.ViewComponents
+{
+    [TestClass]
+    public class LanguageSwitcherViewComponentTests
+    {
+
+        private Mock<HttpContext> _httpContextMock = null!;
+        private Mock<HttpRequest> _httpRequestMock = null!;
+        private IOptions<RequestLocalizationOptions> _localizationOptions = null!;
+        private LanguageSwitcherViewComponent _viewComponent = null!;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+
+            // Set up Localization options
+            var options = new RequestLocalizationOptions();
+            options.AddSupportedCultures(Language.English, Language.Welsh);
+            _localizationOptions = Options.Create(options);
+
+            // Initialize the view component
+            _viewComponent = new LanguageSwitcherViewComponent(_localizationOptions);
+
+            // Set up Mock for HttpContext and HttpRequest
+            _httpContextMock = new Mock<HttpContext>();
+            _httpRequestMock = new Mock<HttpRequest>();
+            _httpRequestMock.Setup(x => x.Path).Returns("/test");
+            _httpRequestMock.Setup(x => x.QueryString).Returns(new QueryString("?test=true"));
+
+            _httpContextMock.Setup(x => x.Request).Returns(_httpRequestMock.Object);
+            _httpContextMock.Setup(x => x.Features.Get<IRequestCultureFeature>())
+                .Returns(new RequestCultureFeature(new RequestCulture(Language.English), null));
+
+            _viewComponent.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext { HttpContext = _httpContextMock.Object }
+            };
+        }
+
+        [TestMethod]
+        public async Task InvokeAsync_ShouldRenderCorrectView_WithExpectedModel()
+        {
+            // Act
+            var result = await _viewComponent.InvokeAsync() as ViewViewComponentResult;
+
+            // Assert
+            using(new AssertionScope())
+            {
+                result.Should().NotBeNull();
+                result!.ViewData!.Model.Should().BeEquivalentTo(new LanguageSwitcherModel
+                {
+                    SupportedCultures = new List<CultureInfo>
+                {
+                    new CultureInfo(Language.English),
+                    new CultureInfo(Language.Welsh)
+                },
+                    CurrentCulture = new CultureInfo(Language.English),
+                    ReturnUrl = "~" + "/test" + "?test=true",
+                    ShowLanguageSwitcher = true
+                });
+            }
+            
+        }
+    }
+}
