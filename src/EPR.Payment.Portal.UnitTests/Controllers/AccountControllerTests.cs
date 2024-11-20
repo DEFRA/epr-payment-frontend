1cee731e41831c945c276cd3cbe58cacc64e3d85
mw-forster <mark.forster@eviden.com>
2024-11-20 09:53:22 +0000

    Fixed and added tests

--- src/EPR.Payment.Portal.UnitTests/Controllers/AccountControllerTests.cs
+++ src/EPR.Payment.Portal.UnitTests/Controllers/AccountControllerTests.cs
@@ -3,7 +3,10 @@ using EPR.Payment.Portal.Common.Configuration;
 using EPR.Payment.Portal.Common.UnitTests.TestHelpers;
 using EPR.Payment.Portal.Controllers;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.FeatureManagement;
 using Moq;
 
 namespace EPR.Payment.Portal.UnitTests.Controllers
@@ -14,17 +17,55 @@ namespace EPR.Payment.Portal.UnitTests.Controllers
 
         [TestMethod, AutoMoqData]
         public void Constructor_ShouldThrowArgumentNullException_WhenDashboardConfigurationIsNull(
-            [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock)
+            [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock,
+            [Frozen] Mock<IFeatureManager> featureManager)
         {
             // Arrange
             _dashboardConfigurationMock.Setup(x => x.Value).Returns((DashboardConfiguration)null!);
+            featureManager.Setup(x => x.IsEnabledAsync("EnableAuthenticationFeature")).Returns(Task.FromResult(true));
 
             // Act
-            Action act = () => new AccountController(_dashboardConfigurationMock.Object);
+            Action act = () => new AccountController(_dashboardConfigurationMock.Object, featureManager.Object);
 
             // Assert
             act.Should().Throw<ArgumentNullException>()
                 .WithMessage("Value cannot be null. (Parameter 'dashboardConfiguration')"); 
+        }
+
+        [TestMethod, AutoMoqData]
+        public void Constructor_ShouldReturnSignOutResult_WhenAuthenticationEnabled(
+            [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock,
+            [Frozen] Mock<IFeatureManager> featureManager)
+        {
+            // Arrange
+            _dashboardConfigurationMock.Setup(x => x.Value).Returns(new DashboardConfiguration { RPDRootUrl = new Service { Url = "http://www.google.com" }, SignOutUrl = new Service { Url = "signout" } });
+            featureManager.Setup(x => x.IsEnabledAsync("EnableAuthenticationFeature")).Returns(Task.FromResult(true));
+
+            AccountController controller = new AccountController(_dashboardConfigurationMock.Object, featureManager.Object);
+
+            // Act
+            var result = controller.SignOut("Https://www.google.com");
+
+            // Assert
+            result.Should().BeOfType<SignOutResult>();
+        }
+
+        [TestMethod, AutoMoqData]
+        public void Constructor_ShouldReturnOK_WhenAuthenticationDisabled(
+            [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock,
+            [Frozen] Mock<IFeatureManager> featureManager)
+        {
+            // Arrange
+            _dashboardConfigurationMock.Setup(x => x.Value).Returns(new DashboardConfiguration { RPDRootUrl = new Service { Url = "http://www.google.com" }, SignOutUrl = new Service { Url = "signout" } });
+            featureManager.Setup(x => x.IsEnabledAsync("EnableAuthenticationFeature")).Returns(Task.FromResult(false));
+
+            AccountController controller = new AccountController(_dashboardConfigurationMock.Object, featureManager.Object);
+
+            // Act
+            var result = controller.SignOut("");
+
+            // Assert
+            result.Should().BeOfType<OkResult>();
         }
     }
 }
