65b698a20051ac94c6dde381cb791bbcebfd412c
Paul Shimmen <paul.shimmen@eviden.com>
2024-12-02 11:39:30 +0000

    Initial changes for implementing cookie banner and tests

---
+++ src/EPR.Payment.Portal/Views/Shared/Components/CookieBanner/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EPR.Payment.Portal.Views.Shared.Components.CookieBanner
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
