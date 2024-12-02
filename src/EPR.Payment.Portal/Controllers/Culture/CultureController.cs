ac93dca5a12a70d40334a83056404334dee4dfed
Manish Agarwal <manish.agarwal.external@eviden.com>
2024-12-02 14:42:35 +0000

    Revert "AD481671 clear cache"
    This reverts commit d46a7559138bb8ee331a2faedd942ac9141850c4.

--- src/EPR.Payment.Portal/Controllers/Culture/CultureController.cs
+++ src/EPR.Payment.Portal/Controllers/Culture/CultureController.cs
@@ -12,9 +12,6 @@ namespace EPR.Payment.Portal.Controllers.Culture
         public LocalRedirectResult UpdateCulture(string culture, string returnUrl)
         {
             HttpContext.Session.SetString(Language.SessionLanguageKey, culture);
-            Response.Headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0";
-            Response.Headers["Pragma"] = "no-cache";
-            Response.Headers["Expires"] = "-1";
             return LocalRedirect(returnUrl);
         }
     }
