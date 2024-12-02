bb0a3a3b0140069106bafdee1012800b7b6641d4
Manish Agarwal <manish.agarwal.external@eviden.com>
2024-12-02 16:21:35 +0000

    tests added

--- src/EPR.Payment.Portal.UnitTests/Controllers/CultureControllerTests.cs
+++ src/EPR.Payment.Portal.UnitTests/Controllers/CultureControllerTests.cs
@@ -1,59 +1,75 @@
-﻿//using EPR.Payment.Portal.Common.Constants;
-//using EPR.Payment.Portal.Controllers.Culture;
-//using FluentAssertions;
-//using FluentAssertions.Execution;
-//using Microsoft.AspNetCore.Http;
-//using Moq;
-//using System.Text;
+﻿using EPR.Payment.Portal.Common.Constants;
+using EPR.Payment.Portal.Controllers.Culture;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Text;
+using System.Text.RegularExpressions;
 
-//namespace EPR.Payment.Portal.UnitTests.Controllers
-//{
-//    [TestClass]
-//    public class CultureControllerTests
-//    {
-//        private const string ReturnUrl = "returnUrl";
-//        private const string CultureEn = "en";
+namespace EPR.Payment.Portal.UnitTests.Controllers
+{
+    [TestClass]
+    public class CultureControllerTests
+    {
+        private const string ReturnUrl = "returnUrl";
+        private const string CultureEn = "en";
 
-//        private Mock<IResponseCookies> _responseCookiesMock = null!;
-//        private Mock<ISession> _sessionMock = null!;
-//        private Mock<HttpContext> _httpContextMock = null!;
-//        private CultureController _systemUnderTest = null!;
+        private Mock<ISession> _sessionMock = null!;
+        private Mock<HttpContext> _httpContextMock = null!;
+        private Mock<HttpResponse> _responseMock = null!;
+        private CultureController _systemUnderTest = null!;
 
-//        [TestInitialize]
-//        public void Setup()
-//        {
-//            _responseCookiesMock = new Mock<IResponseCookies>();
-//            _sessionMock = new Mock<ISession>();
-//            _httpContextMock = new Mock<HttpContext>();
+        private const string CultureKey = Language.SessionLanguageKey;
 
-//            // Set up the controller with mock HttpContext
-//            _systemUnderTest = new CultureController
-//            {
-//                ControllerContext = { HttpContext = _httpContextMock.Object }
-//            };
+        [TestInitialize]
+        public void Setup()
+        {
+            _sessionMock = new Mock<ISession>();
+            _httpContextMock = new Mock<HttpContext>();
+            _responseMock = new Mock<HttpResponse>();
 
-//            _httpContextMock.Setup(x => x.Session).Returns(_sessionMock.Object);
-//        }
+            // Mock Response.Headers
+            var headers = new HeaderDictionary();
+            _responseMock.SetupGet(x => x.Headers).Returns(headers);
 
-//        [TestMethod]
-//        public void CultureController_UpdateCulture_RedirectsToReturnUrlWithCulture()
-//        {
-//            // Arrange
-//            _httpContextMock
-//                .Setup(x => x.Response.Cookies)
-//                .Returns(_responseCookiesMock.Object);
+            // Set up HttpContext
+            _httpContextMock.SetupGet(x => x.Session).Returns(_sessionMock.Object);
+            _httpContextMock.SetupGet(x => x.Response).Returns(_responseMock.Object);
 
-//            var cultureBytes = Encoding.UTF8.GetBytes(CultureEn);
+            // Set up the controller with mock HttpContext
+            _systemUnderTest = new CultureController
+            {
+                ControllerContext = { HttpContext = _httpContextMock.Object }
+            };
 
-//            // Act
-//            var result = _systemUnderTest.UpdateCulture(CultureEn, ReturnUrl);
+        }
 
-//            // Assert
-//            using (new AssertionScope())
-//            {
-//                result.Url.Should().Be(ReturnUrl);
-//                _sessionMock.Verify(x => x.Set(Language.SessionLanguageKey, cultureBytes), Times.Once);
-//            }
-//        }
-//    }
-//}
+        [TestMethod]
+        public void CultureController_UpdateCulture_RedirectsToReturnUrlWithCulture()
+        {
+            // Arrange
+            var cultureBytes = Encoding.UTF8.GetBytes(CultureEn);
+
+            // Act
+            var result = _systemUnderTest.UpdateCulture(CultureEn, ReturnUrl);
+
+            // Assert
+            using (new AssertionScope())
+            {
+                // Verify session set
+                _sessionMock.Verify(x => x.Set(CultureKey, cultureBytes), Times.Once);
+
+                // Verify headers
+                var headers = _responseMock.Object.Headers;
+                headers["Cache-Control"].ToString().Should().Be("no-store, no-cache, must-revalidate, max-age=0");
+                headers["Pragma"].ToString().Should().Be("no-cache");
+                headers["Expires"].ToString().Should().Be("-1");
+
+                // Verify redirection
+                result.Url.Should().Be(ReturnUrl);
+            }
+
+        }
+    }
+}
--- src/EPR.Payment.Portal/Controllers/Culture/CultureController.cs
+++ src/EPR.Payment.Portal/Controllers/Culture/CultureController.cs
@@ -1,12 +1,10 @@
 ﻿using EPR.Payment.Portal.Common.Constants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics.CodeAnalysis;
 
 namespace EPR.Payment.Portal.Controllers.Culture
 {
     [AllowAnonymous]
-    [ExcludeFromCodeCoverage]
     public class CultureController : Controller
     {
         [HttpGet]
