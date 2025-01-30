ff367f629a7143bb932c390be9b8fb29108a82f1
Manish Agarwal <167305073+manishagarwalEviden@users.noreply.github.com>
2025-01-30 15:54:20 +0000

    AD501339 Removed custom health checks (#115)
    * AD501339 Removed custom health checks
    
    * removed proj back up file

--- src/EPR.Payment.Portal/Extension/DependencyExtensions.cs
+++ src/EPR.Payment.Portal/Extension/DependencyExtensions.cs
@@ -9,29 +9,18 @@ namespace EPR.Payment.Portal.Extension
     [ExcludeFromCodeCoverage]
     public static class DependencyExtensions
     {
-        private static readonly string Ready = "ready";
         public static IServiceCollection AddDependencies(
             this IServiceCollection services)
         {
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddHttpContextAccessor();
             services.AddScoped<IPaymentsService, PaymentsService>();
-            services.AddScoped<IPaymentFacadeHealthService, PaymentFacadeHealthService>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<ICookieService, CookieService>();
             services.AddHttpClient();
 
             return services;
 
-        }
-
-        public static IServiceCollection AddServiceHealthChecks(this IServiceCollection services)
-        {
-            services.AddHealthChecks()
-                .AddCheck<PaymentsPortalHealthCheck>(PaymentsPortalHealthCheck.HealthCheckResultDescription,
-                    failureStatus: HealthStatus.Unhealthy,
-                    tags: new[] { Ready });
-            return services;
         }
     }
 }
