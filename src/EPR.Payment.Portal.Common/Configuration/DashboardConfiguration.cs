5d8ec69719505327ae53a6cb5ef3ed74bff8f394
rekhadevieviden <rekha.devi.external@eviden.com>
2025-06-18 10:38:52 +0100

    Added functionality for backurl based on requestor type (#129)

--- src/EPR.Payment.Portal.Common/Configuration/DashboardConfiguration.cs
+++ src/EPR.Payment.Portal.Common/Configuration/DashboardConfiguration.cs
@@ -5,9 +5,11 @@
         public static string SectionName => "Dashboard";
 
         public Service RPDRootUrl { get; set; } = new Service();
+        public Service RepExpRootUrl { get; set; } = new Service();
 
         public Service MenuUrl { get; set; } = new Service();
         public Service BackUrl { get; set; } = new Service();
+        public Service RepExpBackUrl { get; set; } = new Service();
         public Service RegistrationTaskList { get; set; } = new Service();
         public Service FeedbackUrl { get; set; } = new Service();
         public Service OfflinePaymentUrl { get; set; } = new Service();
