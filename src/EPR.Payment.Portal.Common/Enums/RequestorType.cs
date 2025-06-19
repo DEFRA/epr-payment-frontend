cd80e60959912f1dc12fc42bd63dae4dacf946cb
Jignesh Jethwani <jignesh.jethwani.external@atos.net>
2025-06-19 13:28:13 +0100

    Feature/560202 copytext unsuccessful registration accreditation fee (#130)
    * added support to PaymentsService to call V2 api if RequestorType is not null
    
    * corrected failing tests
    
    * added missing unit tests
    
    * Created seperate partial views, one standard and another for reprocessorexporter, move resource files for english and welsh into partial view resources, as reprocess and exporter has different govt pay failure copy text
    
    * updated after PR comments
    
    * corrected use of AssertionScope
    
    * rename httpclient name to use version2 name for v2 service to prevent duplicate http client name
    
    * keep the facadeservice v2 client name consistent epr-config in appsettings
    
    * add line break
    
    * Refactor code based on merge back to main
    
    - Remove Enum Extrensions.  duplicate requestor types enum,  more back url logic to partial view
    
    * removed resource file for Govt Pay Index Page, no longer required
    
    * Updated welsh copy text for body content
    
    ---------
    
    Co-authored-by: Lee Carswell (ext) <lee.carswell.external@eviden.com>

--- src/EPR.Payment.Portal.Common/Enums/RequestorType.cs
+++ src/EPR.Payment.Portal.Common/Enums/RequestorType.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EPR.Payment.Portal.Common.Enums
+﻿namespace EPR.Payment.Portal.Common.Enums
 {
     public enum RequestorType
     {
+        Producers,        
+        ComplianceSchemes,
         Reprocessors,
         Exporters
     }
