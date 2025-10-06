6c1edeb13a2610b50962e202075fec9e73b6f8b3
hamzahshakeel907 <hamzah.shakeel.external@eviden.com>
2025-10-06 15:10:31 +0100

    Revert "Updated govuk-frontend to 5.11.1 (#138)"
    This reverts commit d83d63bd1207e61e83e68a380325dcf067cca8c4.

--- src/EPR.Payment.Portal/gulpfile.js
+++ src/EPR.Payment.Portal/gulpfile.js
@@ -1,60 +1,53 @@
 ﻿const gulp = require('gulp');
 const concat = require('gulp-concat');
-const sass = require('gulp-sass')(require('sass'));
-const path = require('path');
+const sass = require('gulp-sass')(require('sass'))
 
 // Define paths to the SCSS files
 const paths = {
-    govuk: "node_modules/govuk-frontend/dist/govuk/",
     scss: [
-        'assets/scss/all.scss'
+        'assets/scss/components/_language-toggle.scss',
+        'assets/scss/components/_crown-footer.scss',
+        'assets/scss/components/_cookies-media.scss'
     ],
     output: 'wwwroot/css'
 };
 
-const deprecationSuppressions = ["import", "mixed-decls", "global-builtin"];
-
-let loadPaths = [
-    path.join(__dirname, "node_modules"),
-    path.join(__dirname, paths.govuk)
-];
-
-const sassOptions = {
-    loadPaths: loadPaths,
-    outputStyle: 'compressed',
-    quietDeps: true,
-    silenceDeprecations: deprecationSuppressions
-};
+// Task to compile GOV.UK Frontend SCSS
+gulp.task('copy-govuk-styles', function () {
+    return gulp.src('node_modules/govuk-frontend/dist/govuk/*.css')
+        .pipe(concat('govuk-frontend.min.css'))
+        .pipe(gulp.dest('wwwroot/css', { overwrite: true }));
+});
 
 // Task to copy GOV.UK Frontend JavaScript
 gulp.task('copy-govuk-scripts', function () {
-    return gulp.src(path.join(paths.govuk, 'govuk-frontend.min.js'))
+    return gulp.src('node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js')
         .pipe(concat('govuk-frontend.min.js'))
         .pipe(gulp.dest('wwwroot/js', { overwrite: true }));
 });
 
 // Task to copy images
 gulp.task('copy-govuk-images', function () {
-    return gulp.src(path.join(paths.govuk, 'assets/images/*'))
-        .pipe(gulp.dest('wwwroot/images', { overwrite: true }));
+    return gulp.src('node_modules/govuk-frontend/dist/govuk/assets/images/*')
+        .pipe(gulp.dest('wwwroot/assets/images', { overwrite: true }));
 });
 
 // Task to copy images to /payment base folder
 gulp.task('copy-govuk-crown-images', function () {
-    return gulp.src('wwwroot/rebrand/images/*') // Source folder
+    return gulp.src('wwwroot/assets/images/*') // Source folder
         .pipe(gulp.dest('wwwroot/payment/assets/images', { overwrite: true })); // Destination folder
 });
 
 // Task to copy fonts
 gulp.task('copy-govuk-fonts', function () {
-    return gulp.src(path.join(paths.govuk, 'assets/fonts/*'))
-        .pipe(gulp.dest('wwwroot/fonts', { overwrite: true }));
+    return gulp.src('node_modules/govuk-frontend/dist/govuk/assets/fonts/*')
+        .pipe(gulp.dest('wwwroot/assets/fonts', { overwrite: true }));
 });
 
 // Task to copy manifest
 gulp.task('copy-govuk-manifest', function () {
-    return gulp.src(path.join(paths.govuk, 'assets/manifest.json'))
-        .pipe(gulp.dest('wwwroot/', { overwrite: true }));
+    return gulp.src('node_modules/govuk-frontend/dist/govuk/assets/manifest.json')
+        .pipe(gulp.dest('wwwroot/assets', { overwrite: true }));
 
 });
 
@@ -62,16 +55,11 @@ gulp.task('copy-govuk-manifest', function () {
 gulp.task('compile-scss', function () {
     return gulp.src(paths.scss)
         .pipe(concat('application.css'))
-        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest(paths.output));
 });
 
-gulp.task('copy-rebrand', () => {
-    return gulp.src(path.join(paths.govuk, '/assets/rebrand/**/*'), { base: path.join(paths.govuk, '/assets/rebrand') })
-        .pipe(gulp.dest('wwwroot/rebrand'));
-});
-
-// Default task, copy-rebrand must come before copy-govuk-crown-images as the latter copies over an image from the former, so the rebrand folder
-// needs to exist beforehand
-gulp.task('build-frontend', gulp.series('copy-govuk-scripts', 'copy-govuk-images', 'copy-rebrand', 'copy-govuk-crown-images',
+// Default task
+gulp.task('build-frontend', gulp.series('copy-govuk-styles', 'copy-govuk-scripts', 'copy-govuk-images', 'copy-govuk-crown-images',
     'copy-govuk-fonts', 'copy-govuk-manifest', 'compile-scss')); 
+
