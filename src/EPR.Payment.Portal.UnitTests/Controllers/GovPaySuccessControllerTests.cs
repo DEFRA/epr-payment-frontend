d7cb5043bfe86388683bd0912060be87b6884ff7
Daniel Ashton <daniel.ashton@dashkat.uk>
2026-01-07 16:20:11 +0000

    SMAL-200 - Add in reading of org name from claims
    Also corrected casing and parameter naming

--- src/EPR.Payment.Portal.UnitTests/Controllers/GovPaySuccessControllerTests.cs
+++ src/EPR.Payment.Portal.UnitTests/Controllers/GovPaySuccessControllerTests.cs
@@ -1,5 +1,4 @@
-﻿using AutoFixture;
-using AutoFixture.MSTest;
+﻿using AutoFixture.MSTest;
 using EPR.Payment.Portal.Common.Configuration;
 using EPR.Payment.Portal.Common.Constants;
 using EPR.Payment.Portal.Common.Models;
@@ -11,95 +10,152 @@ using FluentAssertions.Execution;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Moq;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Text.Json;
+using EPR.Common.Authorization.Models;
 
-namespace EPR.Payment.Portal.UnitTests.Controllers
+namespace EPR.Payment.Portal.UnitTests.Controllers;
+
+[TestClass]
+public class GovPaySuccessControllerTests
 {
-    [TestClass]
-    public class GovPaySuccessControllerTests
+    [TestMethod, AutoMoqData]
+    public void Constructor_ShouldThrowArgumentNullException_WhenDashboardConfigurationIsNull(
+        [Frozen] Mock<IOptions<DashboardConfiguration>> dashboardConfigurationMock)
     {
+        dashboardConfigurationMock.Setup(x => x.Value).Returns((DashboardConfiguration)null!);
 
-        [TestMethod, AutoMoqData]
-        public void Constructor_ShouldThrowArgumentNullException_WhenDashboardConfigurationIsNull(
-            [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock)
+        Action act = () => _ = new GovPaySuccessController(dashboardConfigurationMock.Object);
+
+        act.Should().Throw<ArgumentNullException>()
+            .WithMessage("Value cannot be null. (Parameter 'dashboardConfiguration')");
+    }
+
+    [TestMethod,AutoMoqData]
+    public void Index_WithInvalidModelState_ShouldRedirectToPaymentError(
+        [Frozen] Mock<IOptions<DashboardConfiguration>> dashboardConfigurationMock,
+        [Frozen] DashboardConfiguration dashboardConfiguration)
+    {
+        dashboardConfigurationMock.Setup(x => x.Value).Returns(dashboardConfiguration);
+        var controller = new GovPaySuccessController(dashboardConfigurationMock.Object);
+        controller.ModelState.AddModelError("Test", "Test error");
+
+        var result = controller.Index(null);
+
+        using (new AssertionScope())
         {
-            // Arrange
-            _dashboardConfigurationMock.Setup(x => x.Value).Returns((DashboardConfiguration)null!);
-
-            // Act
-            Action act = () => _ = new GovPaySuccessController(_dashboardConfigurationMock.Object);
-
-            // Assert
-            act.Should().Throw<ArgumentNullException>()
-                .WithMessage("Value cannot be null. (Parameter 'dashboardConfiguration')");
+            var redirectResult = result.Should().BeOfType<RedirectToRouteResult>().Which;
+            redirectResult.RouteName.Should().Be(RouteNames.GovPay.PaymentError);
+            redirectResult!.RouteValues!["message"].Should().Be(ExceptionMessages.ErrorInvalidViewModel);
         }
+    }
 
-        [TestMethod,AutoMoqData]
-        public void Index_WithInvalidModelState_ShouldRedirectToPaymentError(
-            [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock,
-            [Frozen] DashboardConfiguration _dashboardConfiguration)
+    [TestMethod, AutoMoqData]
+    public void Index_WhenViewModelIsNull_ShouldRedirectToPaymentError(
+        [Frozen] Mock<IOptions<DashboardConfiguration>> dashboardConfigurationMock,
+        [Frozen] DashboardConfiguration dashboardConfiguration)
+    {
+        CompletePaymentViewModel? completePaymentViewModel = null;
+        dashboardConfigurationMock.Setup(x => x.Value).Returns(dashboardConfiguration);
+        var controller = new GovPaySuccessController(dashboardConfigurationMock.Object);
+
+        var result = controller.Index(completePaymentViewModel);
+
+        using (new AssertionScope())
         {
-            // Arrange
-            _dashboardConfigurationMock.Setup(x => x.Value).Returns(_dashboardConfiguration);
-            var controller = new GovPaySuccessController(_dashboardConfigurationMock.Object);
-            controller.ModelState.AddModelError("Test", "Test error");
-
-            // Act
-            var result = controller.Index(null);
-
-            // Assert
-            using (new AssertionScope())
-            {
-                var redirectResult = result.Should().BeOfType<RedirectToRouteResult>().Which;
-                redirectResult.RouteName.Should().Be(RouteNames.GovPay.PaymentError);
-                redirectResult!.RouteValues!["message"].Should().Be(ExceptionMessages.ErrorInvalidViewModel);
-            }
-
+            var redirectResult = result.Should().BeOfType<RedirectToRouteResult>().Which;
+            redirectResult.RouteName.Should().Be(RouteNames.GovPay.PaymentError);
+            redirectResult!.RouteValues!["message"].Should().Be(ExceptionMessages.ErrorInvalidViewModel);
         }
+    }
 
-        [TestMethod, AutoMoqData]
-        public void Index_WhenViewModelIsNull_ShouldRedirectToPaymentError(
-            [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock,
-            [Frozen] DashboardConfiguration _dashboardConfiguration)
-        {
-            // Arrange
-            CompletePaymentViewModel? _completePaymentViewModel = null;
-            _dashboardConfigurationMock.Setup(x => x.Value).Returns(_dashboardConfiguration);
-            var controller = new GovPaySuccessController(_dashboardConfigurationMock.Object);
-
-            // Act
-            var result = controller.Index(_completePaymentViewModel);
-
-            // Assert
-            using (new AssertionScope())
+    [TestMethod, AutoMoqData]
+    public void Index_WithValidModelState_ShouldReturnViewWithCompositeViewModel(
+        string organisationName,
+        [Frozen] Mock<IOptions<DashboardConfiguration>> dashboardConfigurationMock,
+        [Frozen] DashboardConfiguration dashboardConfiguration,
+        [Frozen] CompletePaymentViewModel completePaymentViewModel)
+    {
+        dashboardConfigurationMock.Setup(x => x.Value).Returns(dashboardConfiguration);
+        var controller = new GovPaySuccessController(dashboardConfigurationMock.Object);
+        var userDataJson = JsonSerializer.Serialize(new 
+        { 
+            Organisations = new[]
             {
-                var redirectResult = result.Should().BeOfType<RedirectToRouteResult>().Which;
-                redirectResult.RouteName.Should().Be(RouteNames.GovPay.PaymentError);
-                redirectResult!.RouteValues!["message"].Should().Be(ExceptionMessages.ErrorInvalidViewModel);
-            }
+                new Organisation { Name = organisationName }
+            } 
+        });
+        var claimsIdentity = new ClaimsIdentity(
+        [
+            new Claim(ClaimTypes.UserData, userDataJson)
+        ], "TestAuth");
+        var httpContext = new DefaultHttpContext
+        {
+            User = new ClaimsPrincipal(claimsIdentity)
+        };
+        controller.ControllerContext = new ControllerContext
+        {
+            HttpContext = httpContext
+        };
 
+        var result = controller.Index(completePaymentViewModel);
+
+        using (new AssertionScope())
+        {
+            var viewResult = result.Should().BeOfType<ViewResult>().Which;
+            var model = viewResult.Model.Should().BeOfType<CompositeViewModel>().Which;
+            model.CompletePaymentViewModel.Should().Be(completePaymentViewModel);
+            model.DashboardConfiguration.Should().Be(dashboardConfiguration);
+            model.OrganisationName.Should().Be(organisationName);
         }
+    }
 
-        [TestMethod, AutoMoqData]
-        public void Index_WithValidModelState_ShouldReturnViewWithCompositeViewModel(
-            [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock,
-            [Frozen] DashboardConfiguration _dashboardConfiguration,
-            [Frozen] CompletePaymentViewModel _completePaymentViewModel)
+    [TestMethod, AutoMoqData]
+    public void Index_WithValidModelState_NoUserDataClaim_ShouldSetOrganisationNameNull(
+        [Frozen] Mock<IOptions<DashboardConfiguration>> dashboardConfigurationMock,
+        [Frozen] DashboardConfiguration dashboardConfiguration,
+        [Frozen] CompletePaymentViewModel completePaymentViewModel)
+    {
+        dashboardConfigurationMock.Setup(x => x.Value).Returns(dashboardConfiguration);
+        var controller = new GovPaySuccessController(dashboardConfigurationMock.Object);
+        var claimsIdentity = new ClaimsIdentity([], "TestAuth");
+        var httpContext = new DefaultHttpContext { User = new ClaimsPrincipal(claimsIdentity) };
+        controller.ControllerContext = new ControllerContext { HttpContext = httpContext };
+
+        var result = controller.Index(completePaymentViewModel);
+
+        using (new AssertionScope())
         {
-            // Arrange
-            _dashboardConfigurationMock.Setup(x => x.Value).Returns(_dashboardConfiguration);
-            var controller = new GovPaySuccessController(_dashboardConfigurationMock.Object);
+            var viewResult = result.Should().BeOfType<ViewResult>().Which;
+            var model = viewResult.Model.Should().BeOfType<CompositeViewModel>().Which;
+            model.OrganisationName.Should().BeNull();
+        }
+    }
 
-            // Act
-            var result = controller.Index(_completePaymentViewModel);
+    [TestMethod, AutoMoqData]
+    public void Index_WithValidModelState_EmptyOrganisations_ShouldSetOrganisationNameNull(
+        [Frozen] Mock<IOptions<DashboardConfiguration>> _dashboardConfigurationMock,
+        [Frozen] DashboardConfiguration _dashboardConfiguration,
+        [Frozen] CompletePaymentViewModel _completePaymentViewModel)
+    {
+        _dashboardConfigurationMock.Setup(x => x.Value).Returns(_dashboardConfiguration);
+        var controller = new GovPaySuccessController(_dashboardConfigurationMock.Object);
+        var userDataJson = JsonSerializer.Serialize(new 
+        { 
+            Organisations = new List<Organisation>()
+        });
+        var claimsIdentity = new ClaimsIdentity([new Claim(ClaimTypes.UserData, userDataJson)], "TestAuth");
+        var httpContext = new DefaultHttpContext { User = new ClaimsPrincipal(claimsIdentity) };
+        controller.ControllerContext = new ControllerContext { HttpContext = httpContext };
 
-            // Assert
-            using (new AssertionScope())
-            {
-                var viewResult = result.Should().BeOfType<ViewResult>().Which;
-                var model = viewResult.Model.Should().BeOfType<CompositeViewModel>().Which;
-                model.completePaymentViewModel.Should().Be(_completePaymentViewModel);
-                model.dashboardConfiguration.Should().Be(_dashboardConfiguration);
-            }
+        var result = controller.Index(_completePaymentViewModel);
+
+        using (new AssertionScope())
+        {
+            var viewResult = result.Should().BeOfType<ViewResult>().Which;
+            var model = viewResult.Model.Should().BeOfType<CompositeViewModel>().Which;
+            model.OrganisationName.Should().BeNull();
         }
     }
 }
--- src/EPR.Payment.Portal/Controllers/GovPaySuccessController.cs
+++ src/EPR.Payment.Portal/Controllers/GovPaySuccessController.cs
@@ -1,33 +1,37 @@
-﻿using EPR.Payment.Portal.Common.Configuration;
+﻿using EPR.Common.Authorization.Extensions;
+using EPR.Payment.Portal.Common.Configuration;
 using EPR.Payment.Portal.Common.Constants;
 using EPR.Payment.Portal.Common.Models;
 using EPR.Payment.Portal.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
-namespace EPR.Payment.Portal.Controllers
+namespace EPR.Payment.Portal.Controllers;
+
+[Route("GovPaySuccess", Name = RouteNames.GovPay.PaymentSuccess)]
+public class GovPaySuccessController(IOptions<DashboardConfiguration> dashboardConfiguration) : Controller
 {
-    [Route("GovPaySuccess", Name = RouteNames.GovPay.PaymentSuccess)]
-    public class GovPaySuccessController(IOptions<DashboardConfiguration> dashboardConfiguration) : Controller
+    private readonly DashboardConfiguration _dashboardConfiguration = dashboardConfiguration?.Value
+                                                                      ?? throw new ArgumentNullException(nameof(dashboardConfiguration));
+
+    [HttpGet]
+    public IActionResult Index(CompletePaymentViewModel? completePaymentResponseViewModel)
     {
-        private readonly DashboardConfiguration _dashboardConfiguration = dashboardConfiguration?.Value
-            ?? throw new ArgumentNullException(nameof(dashboardConfiguration));
-
-        [HttpGet]
-        public IActionResult Index(CompletePaymentViewModel? completePaymentResponseViewModel)
+        if (!ModelState.IsValid || completePaymentResponseViewModel is null)
         {
-            if (!ModelState.IsValid || completePaymentResponseViewModel is null)
-            {
-                return RedirectToRoute(RouteNames.GovPay.PaymentError, new { message = ExceptionMessages.ErrorInvalidViewModel });
-            }
-
-            var compositeViewModel = new CompositeViewModel
-            {
-                completePaymentViewModel = completePaymentResponseViewModel,
-                dashboardConfiguration = _dashboardConfiguration
-            };
-
-            return View(compositeViewModel);
+            return RedirectToRoute(RouteNames.GovPay.PaymentError, new { message = ExceptionMessages.ErrorInvalidViewModel });
         }
+
+        var userData = User?.GetUserData();
+        var organisationName = userData?.Organisations?.FirstOrDefault()?.Name;
+
+        var compositeViewModel = new CompositeViewModel
+        {
+            CompletePaymentViewModel = completePaymentResponseViewModel,
+            DashboardConfiguration = _dashboardConfiguration,
+            OrganisationName = organisationName
+        };
+
+        return View(compositeViewModel);
     }
 }
